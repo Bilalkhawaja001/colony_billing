786cf87e5eb3d7fcfaa6b07ba8bda6208dae271e
Bilalkhawaja001 <126321990+Bilalkhawaja001@users.noreply.github.com>
2026-06-23 16:33:26 +0500

    Allow control room readiness month selection

--- laravel_draft/app/Http/Controllers/Billing/ControlRoom/ControlRoomController.php
+++ laravel_draft/app/Http/Controllers/Billing/ControlRoom/ControlRoomController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers\Billing\ControlRoom;
 
 use App\Http\Controllers\Controller;
 use App\Services\Billing\ControlRoom\ReadinessService;
+use Illuminate\Http\Request;
 
 class ControlRoomController extends Controller
 {
-    public function index(ReadinessService $readiness)
+    public function index(Request $request, ReadinessService $readiness)
     {
         return view('billing_control.control_room', [
             'pageTitle' => 'Colony Billing Control Room',
-            'readiness' => $readiness->summary(),
+            'readiness' => $readiness->summary($request->query('month_cycle')),
         ]);
     }
 }
